7bd4cc4c078331cf15ec823fa444e7e86d41493e
eddimull <eddimull@gmail.com>
2020-01-10 15:28:25 -0600

    cleaning up css and js.

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -1,75 +1,61 @@
-<nav class="navbar navbar-expand-sm navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="/">
-            <?php
-                //tis the season.
-                //this reverts the xmas tree logo after 3 days into the new year. If after 3 days, the year is the same it will know that time is up into the new year.
-                // $month = date('m');
-                // $xmas = $month == 12;
-                // if ($month == 1 && date("Y", strtotime("-3 day")) !== date("Y")) {
-                //     $xmas = true;
-                // }
-                // if ($xmas) {
-                //     echo '<img class="brand-image" src="/grfx/Apex_Christmas_Logo_LG_2017.png" alt="Apex Innovations, Education Healthcare Relies On"/>';
-                // } else {
-                //     echo '<img class="brand-image" src="/grfx/ApexLogo500.svg" alt="Apex Innovations, Education Healthcare Relies On"/>';
-                // }
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Apex Innovations
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            ?>
-            <img width="430" height="85" class="brand-image" src="/grfx/apexLogo.png" alt="logo">
-            <img width="45" height="50" class="brand-icon" src="/grfx/starIcon.svg" alt="star icon">
-        </a>
-        
-        <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation"
-        class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div id="navbarSupportedContent" class="navbar-collapse collapse show" style="">
-            <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="/login" class="nav-link">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >Education<span class="caret"></span></a>
-                    <ul class="dropdown-menu">                   
-                        <li><a href="products.html" style="display:block;">All Products</a></li>
-                        <li><a href="products.html#cardiac" style="display:block;">Cardiac Courseware</a></li>
-                        <li><a href="products.html#sepsis" style="display:block;">Sepsis Courseware</a></li>
-                        <li><a href="products.html#neuro" style="display:block;">Neuro Courseware</a></li>
-                        <li><a href="products.html#free" style="display:block;">Free Courseware</a></li>
-                        <li><a href="products.html#mirule" style="display:block;">MI Rule Visions</a></li>      
-                        <li><a href="products.html#bundles" style="display:block;">Courseware Bundles</a></li>      
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link {{Route::currentRouteName() == '' ? 'activeNav' : ''}}" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{Route::is('education') ? 'activeNav' : ''}}" href="/">Education & Tools</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{Route::is('quantum') ? 'activeNav' : ''}}" href="/">Quantum Competency</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{Route::is('about') ? 'activeNav' : ''}}" href="/">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{Route::is('about') ? 'activeNav' : ''}}" href="/">Contact Us</a>
+                        </li>
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                     </ul>
-                </li>
-                <li class="opLink nav-item">
-                    <a class="nav-link" href="/op.html">Competency Validation</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/team.html">Team</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/news.html">News</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/careers.html">Careers</a>
-                </li>
-            </ul>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right text-uppercase text-center">
-                
-            </ul>
-        </div> -->
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
-
-
-    
+                </div>
+            </div>
+        </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 		
-	<div class="row relative">
+	<div class="row relative sm-margin-bot">
     <picture class="centerTrick fullWidth">
       <source media="(max-width: 480px)" srcset="grfx/ipadBG_480.jpg"/>
       <source media="(max-width: 992px)" srcset="grfx/ipadBG_992.jpg"/>
@@ -10,7 +10,6 @@
       <source media="(min-width: 1201px)" srcset="grfx/ipadBGFull.jpg"/>
       <img class="fullWidth centerTrick" id="ipadBG" src="grfx/ipadBGFull.jpg" alt="Apex Innovations"/>
     </picture>
-    <canvas id="pictureCanvas" height="459" width="1209" class="absolute fullHeight fullWidth absoluteTop absoluteLeft"></canvas>
 	</div>
   
   <!-- <div class="row relative sliderContainer text-center" height="auto">	
@@ -18,9 +17,12 @@
     </div>
   </div> -->
 
-  <div class="container-fluid">
-     <div class="row justify-content-sm-center header">  
-       Welcome to Apex Innovations
+  <div class="container-fluid med-margin-top med-margin-bot mdPadBot">
+
+     <div class="row justify-content-sm-center redBottom">  
+       <h3 class="redBot smPadBot">
+          Welcome to Apex Innovations
+        </h3>
       </div>  
 
       <div class="row justify-content-sm-center"> 
@@ -43,38 +45,55 @@
   <!-- Page Content -->
     
   <div class="container-fluid">
-	  <div class="row grey">
-      <div class="col-sm-12 bottomBorder">
-        <h2>Online Education and Tools</h2>
-      </div>
-
-      <div class="row">
-        <div class="col-sm-6">
+	  <div class="row grayBackground">
+            
+      <div class="col-sm-4">
           <img alt="Product Box" data-550="transform:translateX(-400px);opacity:0;" data-600="transform:translateX(0px);opacity:1;" class="genericGraphic" src="grfx/Impulse3_ProductBox_R.png"/>
+      </div>
+      <div class="col-sm-8">
+        <div class="row">
+          <div class="col-md-12 med-margin-top">
+              <h2 class="redBot">Online Education and Tools</h2>
+          </div>
         </div>
-
-        <div class="col-sm-6">
-          <h4>imPULSE 3.0</h4>
-          <p>ECG and Chest Pain Competency SeriesNine individual guideline-based courses are designed to address the educational needs of monitor techs to physicians.</p>
+        <div class="row">
+          <div class="col-md-12">
+              <h4>imPULSE 3.0</h4>
+              <div class="row">
+                ECG and Chest Pain Competency Series
+              </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 sm-margin-top">
+            <p>Nine individual guideline-based courses are designed to address the educational needs of monitor techs to physicians.</p>
+          </div>
         </div>
       </div>
-
   </div>
 
   <!-- Partners -->
   <div class="container-fluid">
-	  <div class="row">
-      <div class="col-sm-4">
-        <img width="150px" src="grfx/BSWH_logo.jpg" alt="Baylor Scott and White" />
-      </div>
+	  <div class="row d-flex flex-wrap align-items-center">
 
-      <div class="col-sm-4">
-        <img width="150px" src="grfx/LifePoint_logo.jpg" alt="LifePoint Health" />
-      </div>
+        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
 
-      <div class="col-sm-4">
-        <img width="150px" src="grfx/NorthShore_logo.jpg" alt="NorthShore" />
-      </div>
+            <img class="img-responsive" src="grfx/BSWH_logo.jpg" style="width:100%" alt="Baylor Scott and White" />
+
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
+
+          <img class="img-responsive" src="grfx/LifePoint_logo.jpg" style="width:100%" alt="LifePoint Health" />
+
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
+
+          <img class="img-responsive" src="grfx/NorthShore_logo.jpg" style="width:100%" alt="NorthShore" />
+
+        </div>
+
     </div>
   </div>
 
@@ -95,14 +114,50 @@
         </div>
       </div>
   </div>
-    
   <!-- Customer Testimonials -->
   <div id="carouselContainer" class="container">
     <div class="row">
 		  
       <div class='col-sm-12'>
         <div class="carousel slide" data-ride="carousel" id="quote-carousel">
-          
+         
+          <!-- Carousel Slides / Quotes -->
+          <div class="carousel-inner text-center">
+
+            <!-- Quote 1 -->
+            <div class="carousel-item active">
+              <div class="testimonialPanel">
+                <p>"So we have been a PSC since 2012. We have never given t-PA in less than 71 minutes, until last week!  We had a 59 minute time.  I think all of the additional education is really paying off!  Thanks for the help"</p>
+                <small class="testimonialText">Beth Wiese - RN Stroke Program Coordinator</small>
+              </div>
+            </div>
+            
+            <!-- Quote 2 -->
+            <div class="carousel-item">
+              <div class="testimonialPanel">
+                <p>"I think it's a great way to have an interactive view of anatomy... then they understand neuro."</p>
+                <small class="testimonialText">Debbie Summers - RN, MSN, ACNS-BC</small>
+              </div>
+            </div>
+            
+            <!-- Quote 3 -->
+            <div class="carousel-item">  
+              <div class="testimonialPanel">
+                <p>"It's so easy to use! It's very clear how to manuever around. Just a click and you can find out information with what's happening in the anatomy."</p>
+                <small class="testimonialText">Rachel Malloy - CNRN</small>
+              </div>
+            </div>
+            
+            <!-- Quote 4 -->
+            <div class="carousel-item">  
+              <div class="testimonialPanel">
+                <p>"Each program actually addresses various learning styles for the participant. So it uses audio, visual, and even tactile learning styles so that each individual person can gain the maximum benefit out of using the program."</p>
+                <small class="testimonialText">Christie Artuso - Ed D, RN, CNRN</small>
+              </div>
+            </div>
+
+        </div>
+
           <!-- Bottom Carousel Indicators -->
           <ol class="carousel-indicators">
             <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
@@ -111,47 +166,11 @@
             <li data-target="#quote-carousel" data-slide-to="3"></li>
           </ol>
 
-          <!-- Carousel Slides / Quotes -->
-          <div class="carousel-inner text-center">
-
-            <!-- Quote 1 -->
-            <div class="item active">
-              <div class="testimonialPanel">
-                <p>"So we have been a PSC since 2012. We have never given t-PA in less than 71 minutes, until last week!  We had a 59 minute time.  I think all of the additional education is really paying off!  Thanks for the help"</p>
-                <small class="testimonialText">Beth Wiese - RN Stroke Program Coordinator</small>
-              </div>
-            </div>
-            
-            <!-- Quote 2 -->
-            <div class="item">
-              <div class="testimonialPanel">
-                <p>"I think it's a great way to have an interactive view of anatomy... then they understand neuro."</p>
-                <small class="testimonialText">Debbie Summers - RN, MSN, ACNS-BC</small>
-              </div>
-            </div>
-            
-            <!-- Quote 3 -->
-            <div class="item">  
-              <div class="testimonialPanel">
-                <p>"It's so easy to use! It's very clear how to manuever around. Just a click and you can find out information with what's happening in the anatomy."</p>
-                <small class="testimonialText">Rachel Malloy - CNRN</small>
-              </div>
-            </div>
-            
-            <!-- Quote 4 -->
-            <div class="item">  
-              <div class="testimonialPanel">
-                <p>"Each program actually addresses various learning styles for the participant. So it uses audio, visual, and even tactile learning styles so that each individual person can gain the maximum benefit out of using the program."</p>
-                <small class="testimonialText">Christie Artuso - Ed D, RN, CNRN</small>
-              </div>
-            </div>
-
-        </div>
       </div>
 		  
 	  </div>
   </div>
-   
+
   <!--<script>
     $('document').ready(function(){
       $('#myModal').modal();
